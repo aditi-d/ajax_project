f757e9b191bd524b239183ac7b47aa9681951d91
aditi.d <adz.988@gmail.com>
2015-12-31 00:14:09 -0800

    Adding Ajax based project

--- js/script.js
+++ js/script.js
@@ -11,59 +11,31 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    var streetStr = $('#street').val();
-    var cityStr = $('#city').val();
-    var address = streetStr + ', ' + cityStr;
-
-    $greeting.text('So, you want to live at ' + address + '?');
-
     // load streetview
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
-    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
+    // YOUR CODE GOES HERE!
+    var $street = $('#street').val();
+    var $city = $('#city').val();
+    var $address = $street + ", " + $city + '';
 
-    // load nytimes
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=3abc9a3d23e60b38c21b4ab9b0a91c07:17:69911633'
-    $.getJSON(nytimesUrl, function(data){
+    var urlLink = "http://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + $address;
+    
+    $body.append('<img class = "bgimg" src="' + urlLink + '">');
+    $greeting.text('So, you want to live at ' + $address);
 
-        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+    var nytimesURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + $city + "&sort=newest&api-key=b91e2d4d50ca5865b0fea09026d1804d:16:73338684 ";
 
-        articles = data.response.docs;
-        for (var i = 0; i < articles.length; i++) {
-            var article = articles[i];
-            $nytElem.append('<li class="article">'+
-                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-                '<p>' + article.snippet + '</p>'+
-            '</li>');
-        };
+    $nytHeaderElem.text("News for " + $city);
 
-    }).error(function(e){
-        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
-    });
-
-
-
-    // load wikipedia data
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
-    var wikiRequestTimeout = setTimeout(function(){
-        $wikiElem.text("failed to get wikipedia resources");
-    }, 8000);
-
-    $.ajax({
-        url: wikiUrl,
-        dataType: "jsonp",
-        jsonp: "callback",
-        success: function( response ) {
-            var articleList = response[1];
-
-            for (var i = 0; i < articleList.length; i++) {
-                articleStr = articleList[i];
-                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
-            };
-
-            clearTimeout(wikiRequestTimeout);
-        }
+    $.getJSON(nytimesURL, function(data) {
+        var newsList = []; 
+        data.response.docs.forEach(function(news) {
+            newsList.push("<li class='article'> <a href='" + news.web_url + "'>" + news.headline.main + "</a>"
+                + "<p>" + news.snippet + "</p></li>");
+        });
+        $nytElem.append(newsList);
+    }).error(function() {
+            $nytHeaderElem.text("Error loading data");
     });
 
     return false;
